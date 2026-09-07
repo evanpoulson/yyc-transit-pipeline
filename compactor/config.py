7e32d056620ee841be536fa335bcbfde56d703f9
Evan Poulson <evanjamespoulson@gmail.com>
2026-09-07 14:34:58 -0600

    Created config

---
+++ compactor/config.py
@@ -0,0 +1,7 @@
+"""Settings for the Calgary Transit GTFS-RT compactor.
+
+Values only. Imported by compactor.py.
+"""
+
+# S3 bucket where raw snapshots are written.
+BUCKET = "yyc-transit-lake-860574615377-ca-central-1-an"
