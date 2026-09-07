aa67b30ab11829a1ef7516583fe4e9b29227c305
Evan Poulson <evanjamespoulson@gmail.com>
2026-09-07 15:16:57 -0600

    boto3 object path listing experimentation

--- compactor/compactor.py
+++ compactor/compactor.py
@@ -1,8 +1,22 @@
+import boto3
+
 from google.transit import gtfs_realtime_pb2
 from google.protobuf.json_format import MessageToDict
+
 import polars as pl
 
-proto = "./compactor/vehiclepositions.pb"
+import config
+
+s3 = boto3.client("s3")
+bucket = config.BUCKET
+prefix='raw/vehicle_positions/'
+
+objects = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)["Contents"] # this is a list of dictionaries
+
+paths = [d.get("Key") for d in objects] # make a list of all the paths gotten from the list of dictionaries
+print(paths)
+
+"""proto = "./compactor/vehiclepositions.pb"
 entities = []
 
 with open(proto, "rb") as f:
@@ -16,4 +30,5 @@ print(entities[0])
 
 df = pl.DataFrame(entities)
 df = df.unnest("vehicle")
-print(df)    
+print(df)  
+"""  
