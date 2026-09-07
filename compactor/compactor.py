fe205846a40541ea503f6d4c092ad8cfd438cf26
Evan Poulson <evanjamespoulson@gmail.com>
2026-09-07 15:50:48 -0600

    Figured out how to get all the snapshot paths from the previous day (target day) using the botos3 client

--- compactor/compactor.py
+++ compactor/compactor.py
@@ -3,31 +3,40 @@ import boto3
 from google.transit import gtfs_realtime_pb2
 from google.protobuf.json_format import MessageToDict
 
+from datetime import datetime, timezone, timedelta
+
 import polars as pl
 
 import config
 
+target_day = (datetime.now(timezone.utc)) - (timedelta(days=1)) # the previous day from when this script is ran, since it'll be run after the end of a day to compact the target day snapshots
+date_path = target_day.strftime("%Y/%m/%d")
+
 s3 = boto3.client("s3")
 bucket = config.BUCKET
-prefix='raw/vehicle_positions/'
+prefix = f"{config.PREFIXES.get("vehicle_positions")}/{date_path}/"
+paginator = s3.get_paginator('list_objects_v2')
 
-objects = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)["Contents"] # this is a list of dictionaries
+page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
 
-paths = [d.get("Key") for d in objects] # make a list of all the paths gotten from the list of dictionaries
-print(paths)
+paths = []
+for page in page_iterator:
+    # 'Contents' won't exist if the prefix or folder is completely empty
+    if 'Contents' in page:
+        for obj in page['Contents']:
+            paths.append(obj['Key'])
 
-"""proto = "./compactor/vehiclepositions.pb"
+"""
 entities = []
+for path in paths:
+    with open(path, "rb") as snapshot:
+        feed = gtfs_realtime_pb2.FeedMessage()
+        feed.ParseFromString(snapshot.read())
 
-with open(proto, "rb") as f:
-    feed = gtfs_realtime_pb2.FeedMessage()
-    feed.ParseFromString(f.read())
+        for entity in feed.entity:
+            entities.append(MessageToDict(entity))"""
 
-    for entity in feed.entity:
-        entities.append(MessageToDict(entity))
-
-print(entities[0])
-
+"""
 df = pl.DataFrame(entities)
 df = df.unnest("vehicle")
 print(df)  
--- compactor/config.py
+++ compactor/config.py
@@ -5,3 +5,10 @@ Values only. Imported by compactor.py.
 
 # S3 bucket where raw snapshots are written.
 BUCKET = "yyc-transit-lake-860574615377-ca-central-1-an"
+
+# File path prefixes for the three different raw GTFS-RT streams stored in the S3 bucket.
+PREFIXES = {
+    "vehicle_positions": "raw/vehicle_positions",
+    "trip_updates": "raw/trip_updates",
+    "service_alerts": "raw/service_alerts",
+}
