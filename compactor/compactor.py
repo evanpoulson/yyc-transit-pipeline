aa439d5378cc32f663e5ea7579b281be9aaad8cd
Evan Poulson <evanjamespoulson@gmail.com>
2026-09-12 17:37:58 -0600

    Implemented method to encapsulate retrieval of object paths

--- compactor/compactor.py
+++ compactor/compactor.py
@@ -9,25 +9,29 @@ from concurrent.futures import ThreadPoolExecutor , as_completed
 
 import config
 
-target_day = (datetime.now(timezone.utc)) - (timedelta(days=1)) # the previous day from when this script is ran, since it'll be run after the end of a day to compact the target day snapshots
-date_path = target_day.strftime("%Y/%m/%d")
-
 s3 = boto3.client("s3")
 bucket = config.BUCKET
 prefix = f"{config.PREFIXES.get("vehicle_positions")}/{date_path}/"
-paginator = s3.get_paginator('list_objects_v2')
 
-page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
+def get_object_paths(s3_client: boto3.client, bucket: str, prefix: str) -> list:
 
-paths = []
-for page in page_iterator:
-    # 'Contents' won't exist if the prefix or folder is completely empty
-    if 'Contents' in page:
-        for obj in page['Contents']:
-            paths.append(obj['Key'])
+    paginator = s3.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
+
+    target_day = (datetime.now(timezone.utc)) - (timedelta(days=1)) # the previous day from when this script is ran, since it'll be run after the end of a day to compact the target day snapshots
+    date_path = target_day.strftime("%Y/%m/%d")
+
+    paths = []
+    for page in page_iterator:
+        # 'Contents' won't exist if the prefix or folder is completely empty
+        if 'Contents' in page:
+            for obj in page['Contents']:
+                paths.append(obj['Key'])
+
+    return paths
 
 # need to get objects from s3
-def get_object(s3_client, bucket: str, key: str) -> bytes:
+def get_object(s3_client: boto3.client, bucket: str, key: str) -> bytes:
 
     response = s3_client.get_object( 
         Bucket=bucket,
@@ -54,29 +58,7 @@ with ThreadPoolExecutor(max_workers=30) as executor:
         key = futures[future]
         try:
             print(parse_snapshot(future.result()))
-            print("\n")
-            print("=" * 100)
-            print("\n")
             #results[key] = data
             #print(f"Downloaded {key} ({len(data)} bytes)")
         except Exception as e:
             print(f"Failed to download {key}: {e}")
-
-#print(results)
-
-"""
-entities = []
-for path in paths:
-    with open(path, "rb") as snapshot:
-        feed = gtfs_realtime_pb2.FeedMessage()
-        feed.ParseFromString(snapshot.read())
-
-        for entity in feed.entity:
-            print(entity)
-            entities.append(MessageToDict(entity))
-
-
-df = pl.DataFrame(entities)
-df = df.unnest("vehicle")
-print(df)  
-"""  
