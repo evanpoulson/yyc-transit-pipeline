586e6e0fd9c49a9346be3980e93f1e66b40e168d
Evan Poulson <evanjamespoulson@gmail.com>
2026-09-12 19:54:22 -0600

    Lots of changes: formatting imports; consistent function spacing, implemented duckdb write to s3 method, developed main method further

--- compactor/config.py
+++ compactor/config.py
@@ -8,7 +8,7 @@ BUCKET = "yyc-transit-lake-860574615377-ca-central-1-an"
 
 # Feed types for the three different raw GTFS-RT streams stored in the S3 bucket.
 FEEDS = {
-    "vehicle_positions": "raw/vehicle_positions",
-    "trip_updates": "raw/trip_updates",
-    "service_alerts": "raw/service_alerts",
+    "vehicle_positions": "vehicle_positions",
+    "trip_updates": "trip_updates",
+    "service_alerts": "service_alerts",
 }
