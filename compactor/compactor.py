95adddf4c32585913b5b1ac7161608193a7cab77
Evan Poulson <evanjamespoulson@gmail.com>
2026-09-13 12:16:07 -0600

    Fixed fetch_snapshots from creating a list[list[dict]], use extend instead of append to now return a list[dict] as intended

--- compactor/compactor.py
+++ compactor/compactor.py
@@ -9,6 +9,8 @@ from google.protobuf.json_format import MessageToDict
 
 import config
 
+import pandas as pd
+
 def get_object_paths(s3_client: boto3.client, bucket: str, feed: str, target_day: datetime) -> list[str]:
 
     date_path = target_day.strftime("%Y/%m/%d")
@@ -53,36 +55,40 @@ def parse_snapshot(snapshot: bytes) -> list[dict]:
 def fetch_snapshots(executor: ThreadPoolExecutor, s3_client: boto3.client, bucket: str, paths: list[str]):
 
     snapshots = []
-    with executor:
+    futures = {executor.submit(get_object, s3_client=s3_client, bucket=bucket, key=key): key for key in paths}
+    for future in as_completed(futures):
 
-        futures = {executor.submit(get_object, s3_client=s3_client, bucket=bucket, key=key): key for key in paths}
-        for future in as_completed(futures):
-
-            object_key = futures[future]
-            try:
-                data = parse_snapshot(future.result())
-                snapshots.append(data)
-                #print(f"Downloaded {object_key} ({len(data)} entities)")
-            except Exception as e:
-                print(f"Failed to download {object_key}: {e}")
+        object_key = futures[future]
+        try:
+            data = parse_snapshot(future.result())
+            snapshots.extend(data)
+            #print(f"Downloaded {object_key} ({len(data)} entities)")
+        except Exception as e:
+            print(f"Failed to download {object_key}: {e}")
 
     return snapshots
 
 def write_curated(db: duckdb.DuckDBPyConnection, bucket: str, rows: list[dict], feed: str) -> None:
 
-    df = pa.Table.from_pylist(rows) 
+    #pd.set_option("display.max_columns", None)     # show every column, no ... in the middle
+    #pd.set_option("display.width", None)           # don't wrap to terminal width
+    #pd.set_option("display.max_colwidth", None)   
 
+    df = pa.Table.from_pylist(rows) 
+    #print(df.slice(0, 10).to_pandas())
+
+    """
     target_day = (datetime.now(timezone.utc) - timedelta(days=1)).strftime("%Y/%m/%d")
     write_path = f"s3://{bucket}/curated/{feed}/date={target_day}/data.parquet"
 
     db.execute(
     "COPY (SELECT DISTINCT * FROM df) TO ? (FORMAT parquet)",
     [write_path],
-    )
+    )"""
 
 def main() -> None:
 
-    feed = config.FEEDS.get("vehicle_positions)")
+    feed = config.FEEDS.get("vehicle_positions")
     bucket = config.BUCKET
     target_day = (datetime.now(timezone.utc) - timedelta(days=1))
 
@@ -103,13 +109,12 @@ def main() -> None:
                 REGION 'ca-central-1'
             )
         """)
-
-        with ThreadPoolExecutor(max_workers=30) as pool:
+  
+        with ThreadPoolExecutor(max_workers=40) as pool:
             paths = get_object_paths(s3_client=s3, bucket=bucket, feed=feed, target_day=target_day)
             vehicle_positions = fetch_snapshots(executor=pool, s3_client=s3, bucket=bucket, paths=paths)
 
         write_curated(db=con, bucket=bucket, rows=vehicle_positions, feed=feed)
-
 
 if __name__ == "__main__":
     main()
